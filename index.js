7d26887d6a7a4c1a0a36be7b02347c1f753bea7a
Natalia Teixeira <nataliat1026@gmail.com>
2022-04-10 17:10:41 -0400

    commit tree 01a6fb7d55201a9bd0c0f535ed664927d5b68d30

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const writeCards = (name, event) => {
+    const newCards = [];
+    for (let i = 0; i < name.length; i++) {
+        newCards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return newCards; 
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], `birthday`);
+
+const countDown = (i) => {
+    while (i >= 0) {
+        console.log(i);
+        i--
+    }
+}
